016b07f0e4159569249dcfcbb35c22948851cb22
kostya-alch <kostafrompiter@gmail.com>
2021-12-21 00:00:19 +0300

    add redux and store, add favicon

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  currentUser: {},
+  isAuth: false,
+};
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
