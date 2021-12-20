7f66feb55359f27f3908956a7a461972c53cd004
kostya-alch <kostafrompiter@gmail.com>
2021-12-20 18:46:55 +0300

    add model user and conneсt to db

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('config');
+const config = require('config'); // подключили все зависимости
 
 const app = express();
 const PORT = config.get('serverPort');
-const start = () => {
+
+const start = async () => { 
   try {
+    mongoose.connect(config.get('dbUrl'));
     app.listen(PORT, () => {
-      console.log('server ready');
+      console.log(`server ready on ${PORT}`);
     });
   } catch (error) {}
-};
+}; // коннект к базе данных
 
 start();
