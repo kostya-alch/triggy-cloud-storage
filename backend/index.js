ec74c1f1c1a8f7fa1423d6489b6c830654dd96ba
kostya-alch <kostafrompiter@gmail.com>
2021-12-20 17:43:40 +0300

    add start server and add config

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log('work');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = config.get('serverPort');
+const start = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log('server ready');
+    });
+  } catch (error) {}
+};
+
+start();
